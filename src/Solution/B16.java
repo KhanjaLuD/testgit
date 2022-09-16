286a87d2699d2fc8a940a4aac12b3d177e477438
Admin <khanjalu.prv@gmail.com>
2022-09-16 15:33:43 +0700

    Đếm số lượng chữ số N

--- src/Solution/B16.java
+++ src/Solution/B16.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class B16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        long n = sc.nextLong();
         long count = 0;
         while (n != 0){
              count++;
               n /= 10;
          }
+        if (count == 0){
+            count++;
+        }
         System.out.println(count);
     }
 }
