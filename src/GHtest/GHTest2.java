25b61a30e8bd35bd0c5d24265070a43ff5531941
Admin <khanjalu.prv@gmail.com>
2022-09-16 14:54:53 +0700

    first time commit

---
+++ src/GHtest/GHTest2.java
@@ -0,0 +1,11 @@
+package GHtest;
+
+import java.util.Scanner;
+
+public class GHTest2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i);
+    }
+}
