daaa9c3c7fa4f164bc44354c7ffeebe72d9e9f16
Admin <khanjalu.prv@gmail.com>
2022-09-16 14:53:45 +0700

    first time commit

--- src/GHtest/GHTEST.java
+++ src/GHtest/GHTEST.java
@@ -1,8 +1,13 @@
 package GHtest;
 
 
+import java.util.Scanner;
+
 public class GHTEST {
     public static void main(String[] args) {
         System.out.println(" Hello World");
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
     }
 }
