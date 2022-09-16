5128ea031618dcc07759722a63d9971f8d0216e6
Admin <khanjalu.prv@gmail.com>
2022-09-16 15:31:07 +0700

    Giai thừa

--- src/Solution/B16.java
+++ src/Solution/B16.java
@@ -2,16 +2,15 @@ package Solution;
 
 import java.util.Scanner;
 
-public class B15 {
+public class B16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long count = 1;
-        for (int i = 0; i <= n; i++) {
-            count ++;
-            n /= 10;
-        }
-
+        long count = 0;
+    while (n != 0){
+        count++;
+        n /= 10;
+    }
         System.out.println(count);
     }
 }
